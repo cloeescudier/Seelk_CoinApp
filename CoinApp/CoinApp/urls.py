8aaefe00e14863127b412f8be42bdf431ec01034
Cloé Escudier <cloe.escudier@epita.fr>
2021-07-16 13:37:47 +0200

    Ajoutez des fichiers projet.

---
+++ CoinApp/CoinApp/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for CoinApp.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', CoinApp.views.home, name='home'),
+    # url(r'^CoinApp/', include('CoinApp.CoinApp.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
